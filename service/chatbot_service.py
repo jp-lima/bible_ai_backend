88daedc0ebbf69a5ba2c740118985878afb6e0a6
joao_pedro <jpbrasil763@gmail.com>
2026-03-11 20:00:16 -0300

    feat: uso de I.A tira um crédito da carteira

--- service/chatbot_service.py
+++ service/chatbot_service.py
@@ -1,7 +1,7 @@
 from locale import windows_locale
 import re
 from integrations.groq import integration_groq 
-from repositories.wallet_repo import get_one_wallet_by_uuid
+from repositories.wallet_repo import get_one_wallet_by_uuid, put_value_in_wallet_by_user_id  
 
 def chatbot_message_service(user_id:str, prompt:str,religion:str,mood:str,need:str  ):
 
@@ -11,6 +11,10 @@ def chatbot_message_service(user_id:str, prompt:str,religion:str,mood:str,need:s
         return
 
     print(religion, mood, need)
+
+
+
+    put_value_in_wallet_by_user_id(wallet[0]["balance"]- 1, user_id) 
             
     message = integration_groq(prompt, religion, need, mood ) 
 
